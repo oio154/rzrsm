cafb32fad8dc0b6f2a55e082520edfbe1edc3fb1
oio154 <jacek@web.katowice.pl>
2017-01-21 09:20:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+    $rand=rand(100000,9999999);
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="css/main.css?<?= $rand ?>" rel="stylesheet">
+    
+    
+    
+    
+</head>
+
+    <body>
+        <div class="hd-part1">
+        <img src="./img/path4147-1059.png" alt="header" class="hd-image" />
+        <img src="./img/zwiazek_BT.png" alt="logo" class="hd-image-logo" />
+            <div class="hd-div-right">Regionalny Związek Rewizyjny Spółdzielczości Mieszkaniowej <span class="nobr">w Katowicach</span></div>
+    </div>
+    <div class="hd-part2">
+        <ul class="menu-ul">
+            <li class="menu-li"><a href="#">O Związku Rewizyjnym</a></li>
+            <li class="menu-li"><a href="#">Prawo i dokumenty</a></li>
+            <li class="menu-li"><a href="#">Menu link 3</a></li>
+            <li class="menu-li"><a href="#">Menu link 4</a></li>
+            <li class="menu-li"><a href="#">Menu link 5</a></li>
+            
+        </ul>    
+    </div>
+
+   
+    
+    <div class="news">
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/160/167/nightlife" /><h1>Wielka uroczystość.</h1></div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/260/266/nature" /><h1>Rodzinny zjazd spółdzielni mieszkaniowych.</h1></div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/165/165/people" /><h1>Zmiany we władzach Związku</h1></div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/166/164/sports" /><h1>Zawody na koniec roku</h1></div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/163/163/" /><h1>Przykłądowy bardzo długi tytuł newsa do sprawdzenia okna tytułowego w którym tekst może się nie zmieścić w całości.</h1></div>
+        <!--div class="news-one"><img class="news-image" src="http://lorempixel.com/164/164/" />oosk</div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/168/168/" />odoo</div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/166/167/" />pldo</div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/162/166/" />plsp</div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/166/165/" />oosk</div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/165/162/" />odoo</div>
+        <div class="news-one"><img class="news-image" src="http://lorempixel.com/168/163/" />pldo</div-->
+    </div>
+        
+        
+        txt
+</body>
+<!--
+
+https://unsplash.it/500/300/?random
+
+
+-->
+</html>
